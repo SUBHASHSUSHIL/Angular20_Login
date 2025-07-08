9a10601481bd1a7a982c263e8951fb89cb316295
SUSHIL THAKUR <mt3028788@gmail.com>
2025-07-09 00:18:18 +0530

    login page dynamic routing

--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet,RouterLink],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
 export class Layout {
+
+  router = inject(Router);
+  onLogoff() {
+    this.router.navigate(['/login']);
+  }
 
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
+  loginForm: FormGroup = new FormGroup({
+    EmailId: new FormControl(""),
+    Password: new FormControl("")
+  });
+
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  onLogin(){
+    debugger;
+    const formValue = this.loginForm.value;
+    this.http.post("https://freeapi.miniprojectideas.com/api/User/Login",formValue).subscribe({
+      next:(response:any) => {
+        debugger;
+        if(response.result)
+        { 
+          alert("Login successful");
+          this.router.navigateByUrl("/dashboard");
+        } else {
+          alert(response.message);
+        }
+      },
+      error:(error) => {
+        debugger;
+        alert(error.error);
+      }
+    })
+  }
 
 }
