15158cf0633450ced67113433992ae80b9d62d36
SUSHIL THAKUR <mt3028788@gmail.com>
2025-07-09 12:19:41 +0530

    complete this tutorial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Layout } from './pages/layout/layout';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     {
@@ -16,10 +17,12 @@ export const routes: Routes = [
     {
         path: '',
         component: Layout,
+        canActivate: [authGuard], // Using authGuard to protect the layout route
         children: [
             {
                 path: 'dashboard',
                 component: Dashboard
+                //canDeactivate: [authGuard] // Using authGuard to protect the dashboard route
             }
         ]
     }
--- src/app/guards/auth-guard.ts
+++ src/app/guards/auth-guard.ts
@@ -1,5 +1,22 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  debugger;
+  console.log('AuthGuard: Checking authentication status...');
+  // Here you would typically check if the user is authenticated
+
+  const router = inject(Router);
+
+  const isLoggedIn = localStorage.getItem('angular20token');
+  if(isLoggedIn != null)
+  {
+    return true;
+  }
+  else
+  {
+    router.navigateByUrl('/login');
+    return false;
+  }
   return true;
 };
